39b5de5963e7aee8dbcb154728d0755764274b50
Justin Charles <charlesjustin2124@gmail.com>
2025-04-10 14:13:57 +0530

    configure tailwind
    Signed-off-by: Justin Charles <charlesjustin2124@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import AmountInput from './AmountInput';
+
+function App() {
+  const [amount, setAmount] = useState('100')
+  return (
+    <main className="max-w-2xl mx-auto px-4 py-8"><h1 className='uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30% '>Find Cheapest BTC</h1>
+    <div className="flex justify-center mt-8">
+      <AmountInput value={amount} onChange={e => setAmount(e.target.value)} />
+    </div>
+    </main>
+  )
+}
+
+export default App
